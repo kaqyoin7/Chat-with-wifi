56004cdc3b302644a3728767df16cdf04621efe2
kaqyoin <kaquoin@163.com>
2024-06-21 11:38:18 +0800

    Optimize multicast by encapsulating thread pool

---
+++ app/src/main/java/com/example/wifichat/util/MulticastThreadPool.java
@@ -0,0 +1,52 @@
+package com.example.wifichat.util;
+
+
+
+import com.example.wifichat.multicast.MulticastReceiver;
+import com.example.wifichat.multicast.MulticastSender;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MulticastThreadPool {
+
+    private static ExecutorService executorService;
+
+    // 初始化线程池
+    public static void init() {
+        executorService = Executors.newCachedThreadPool();
+    }
+
+    // 提交接收组播消息任务给线程池执行
+    public static void submitReceiverTask(final MulticastReceiver receiver) {
+        if (executorService == null) {
+            init();
+        }
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                receiver.receiveMulticastMessage();
+            }
+        });
+    }
+
+    // 提交发送组播消息任务给线程池执行
+    public static void submitSenderTask(final MulticastSender sender, final String message) {
+        if (executorService == null) {
+            init();
+        }
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                sender.sendMulticastMessage(message);
+            }
+        });
+    }
+
+    // 关闭线程池
+    public static void shutdown() {
+        if (executorService != null) {
+            executorService.shutdown();
+        }
+    }
+}
